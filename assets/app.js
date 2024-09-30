e66dd8b3397e6197478a637a44c85e2f55975054
Martial Mendes <martial.mendes@gmail.com>
2024-09-30 11:34:44 +0200

    template

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,7 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+import Alpine from 'alpinejs'
+windows.Alpine = Alpine
+Alpine.start()
---
+++ src/Controller/CvController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CvController extends AbstractController
+{
+    #[Route('/', name: 'app_cv')]
+    public function index(): Response
+    {
+
+        $projectDir = $this->getParameter('kernel.project_dir');
+
+        $json = file_get_contents($projectDir.'/import/cv.json');
+
+        $cv = json_decode($json, true);
+
+
+
+        return $this->render('cv/index.html.twig', [
+            'cv' => $cv,
+        ]);
+    }
+}
